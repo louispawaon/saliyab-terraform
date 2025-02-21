bc39c051beefe110580da9475f63043661eda282
louispawaon <lpawaon@gmail.com>
2025-02-21 15:18:50 +0800

    refactor: change in workflow

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
           className="h-[40vmin] pointer-events-none animate-[spin_20s_linear_infinite]"
           alt="logo"
         />
-        <p className="text-[#7B42BC] font-bold">Saliyab Terraform 🎉</p>
+        <p className="text-[#7B42BC] font-bold">Migs Hatdog🎉</p>
       </header>
     </div>
   );
